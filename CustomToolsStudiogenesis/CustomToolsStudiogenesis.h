737de8cc2409e353f56cff9aa537c971d7379e8a
Jorge Gómez <jorge.gomez@studiogenesis.es>
2021-04-28 12:34:04 +0200

    Initial Commit

---
+++ CustomToolsStudiogenesis/CustomToolsStudiogenesis.h
@@ -0,0 +1,18 @@
+//
+//  CustomToolsStudiogenesis.h
+//  CustomToolsStudiogenesis
+//
+//  Created by Studiogenesis on 28/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomToolsStudiogenesis.
+FOUNDATION_EXPORT double CustomToolsStudiogenesisVersionNumber;
+
+//! Project version string for CustomToolsStudiogenesis.
+FOUNDATION_EXPORT const unsigned char CustomToolsStudiogenesisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomToolsStudiogenesis/PublicHeader.h>
+
+
