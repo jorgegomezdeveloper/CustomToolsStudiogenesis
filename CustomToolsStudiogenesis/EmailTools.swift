47dcb999ffc7a7036b5a6510311227acf734f3a6
Jorge Gómez <jorge.gomez@studiogenesis.es>
2021-04-28 16:53:38 +0200

    New class added.

---
+++ CustomToolsStudiogenesis/EmailTools.swift
@@ -0,0 +1,27 @@
+//
+//  EmailTools.swift
+//  CustomToolsStudiogenesis
+//
+//  Created by David Galán on 28/07/2019.
+//  Copyright © 2019 David Galán. All rights reserved.
+//
+
+import SwiftUI
+
+extension String {
+
+    public func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+
+    public func openEmail() {
+        if self.isValidEmail() {
+            if let url = URL(string: "mailto:\(self)") {
+                UIApplication.shared.open(url)
+            }
+        }
+    }
+
+}
---
+++ CustomToolsStudiogenesis/ScrollViewTools.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewTools.swift
+//  CustomToolsStudiogenesis
+//
+//  Created by David SG on 28/07/2020.
+//  Copyright © 2020 David Galán. All rights reserved.
+//
+
+import SwiftUI
+
+extension UIScrollView {
+
+    public func scrollTo(horizontalPage: Int? = 0, verticalPage: Int? = 0, animated: Bool? = true) {
+        var frame: CGRect = self.frame
+        frame.origin.x = frame.size.width * CGFloat(horizontalPage ?? 0)
+        frame.origin.y = frame.size.width * CGFloat(verticalPage ?? 0)
+        self.scrollRectToVisible(frame, animated: animated ?? true)
+    }
+
+}
